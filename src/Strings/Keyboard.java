759a6c53364ca71c47db5b321c72fdca948c8579
MehulPrince <mhulprnce@gmail.com>
2024-03-10 20:21:07 +0530

    Keyboard_string_problem

--- src/Strings/Keyboard.java
+++ src/Strings/Keyboard.java
@@ -1,21 +1,150 @@
-package Strings;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class Keyboard {
-    public static void main(String[] args){
-        char[] row1 = {'q','w','e','r','t','t','y','y','u','i','o','p'};
-        char[] row2 = {'a','s','d','f','g','h','j','k','l',};
-        char[] row3 = {'z','x','c','v','b','n','m'};
+class DoublyLinkedListNode {
+    public int data;
+    public DoublyLinkedListNode next;
+    public DoublyLinkedListNode prev;
 
-        int n;
+    public DoublyLinkedListNode(int nodeData) {
+        this.data = nodeData;
+        this.next = null;
+        this.prev = null;
+    }
+}
 
-        Scanner sc  = new Scanner(System.in);
-        System.out.println("Enter Words");
-        String[] UserWords = new String[n];{sc.nextLine();
-        for (int i = 0;i<n;i++){
-             if(Arrays.toString(UserWords).charAt(i))
+class DoublyLinkedList {
+    public DoublyLinkedListNode head;
+    public DoublyLinkedListNode tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertNode(int nodeData) {
+        DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+        }
+
+        this.tail = node;
+    }
+}
+
+class DoublyLinkedListPrintHelper {
+    public static void printList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'reverse' function below.
+     *
+     * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+     * The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+
+    public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist) {
+        // Write your code here
+        Node Reverse(Node head) {
+
+            if(head==null)
+                return null;
+
+            if(head.next==null)
+                return head;
+
+            Node temp=head;
+            Node next=temp.next;
+            while(next!=null)
+            {
+
+                temp.next=temp.prev;
+                temp.prev=next;
+                temp=next;
+                next=next.next;
+
+            }
+
+            temp.next=temp.prev;
+            temp.prev=null;
+            return temp;
+
         }
 
 
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                DoublyLinkedList llist = new DoublyLinkedList();
+
+                int llistCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+                IntStream.range(0, llistCount).forEach(i -> {
+                    try {
+                        int llistItem = Integer.parseInt(bufferedReader.readLine().trim());
+
+                        llist.insertNode(llistItem);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                DoublyLinkedListNode llist1 = Result.reverse(llist.head);
+
+                DoublyLinkedListPrintHelper.printList(llist1, " ", bufferedWriter);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
