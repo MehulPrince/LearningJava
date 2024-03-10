a63ec0af1324ec63f1bf8b855d927ff96f2417c9
MehulPrince <mhulprnce@gmail.com>
2024-03-10 20:17:16 +0530

    ArrayLIst basic-1

---
+++ src/ArrayList/ArrayLists.java
@@ -0,0 +1,16 @@
+package ArrayList;
+import java.util.ArrayList;
+
+ class ArrayLists {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+    //Adding Elements
+
+        list.add(0);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+    }
+}
